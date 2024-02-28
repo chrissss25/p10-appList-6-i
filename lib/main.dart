88500ff0d6df7c840cb6f97b246d8f45d5c3624d
CHRISTIAN_DAVID_HEREDIA_HEREDIA <a.21308051280367@cbtis128.edu.mx>
2024-02-28 16:49:17 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiListaCard());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiListaCard extends StatelessWidget {
+  const MiListaCard({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: "app list view",
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PaginaInicio(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class PaginaInicio extends StatefulWidget {
+  const PaginaInicio({Key? key}) : super(key: key);
 
+  @override
+  State<PaginaInicio> createState() => _PaginaInicioState();
+}
+
+class _PaginaInicioState extends State<PaginaInicio> {
+  List<String> images = [
+    "assets/images/a1.jpg",
+    "assets/images/a2.png",
+    "assets/images/a3.png",
+    "assets/images/a4.jpg",
+    "assets/images/a5.jpg",
+    "assets/images/a6.jpg",
+    "assets/images/a7.png",
+    "assets/images/a8.jpg",
+    "assets/images/a9.png",
+    "assets/images/a10.jpg",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: const Text("list view christian heredia"),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext, index) {
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(images[index]),
+              ),
+              title: Text("This is title"),
+              subtitle: Text("This is subtitle"),
+            ),
+          );
+        },
+        itemCount: images.length,
+        shrinkWrap: true,
+        padding: EdgeInsets.all(5),
+        scrollDirection: Axis.vertical,
       ),
     );
   }
